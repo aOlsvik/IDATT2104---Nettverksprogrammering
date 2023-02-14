a5362b8d0ed40eadf851b0ef30cac864cc4b48e5
Aleksander Olsvik <alekol@ntnu.no>
2023-02-14 13:54:08 +0100

    Finished task 3

---
+++ Øving 3/ClientThread.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientThread extends Thread{
+
+    private final Socket connection;
+    public ClientThread(Socket socket){
+        this.connection=socket;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStreamReader readConnection
+                    = new InputStreamReader(connection.getInputStream());
+            BufferedReader reader = new BufferedReader(readConnection);
+            PrintWriter printer = new PrintWriter(connection.getOutputStream(), true);
+
+
+            /* Confirm communication to the client */
+            System.out.println("Contact was made with the client!");
+            printer.println("Contact was made with the server!");
+            printer.println("Do you wanna add or subtract?");
+            printer.println("1. Add");
+            printer.println("2. Subtract");
+            printer.println("3. Exit");
+
+            /* Receive data from client */
+            String operation = reader.readLine();
+            int selection;
+            int firstNumber;
+            int secondNumber;
+
+            while (true) {
+                selection = Integer.parseInt(operation.trim());
+                if(selection == 3){
+                    break;
+                }
+
+                printer.println("First number:");
+                String first = reader.readLine();
+                firstNumber = Integer.parseInt(first);
+
+                printer.println("Second number:");
+                String second = reader.readLine();
+                secondNumber = Integer.parseInt(second);
+
+                if(selection == 1) printer.println(firstNumber + " + "+ secondNumber + " = " + (firstNumber+secondNumber));
+                else printer.println(firstNumber + " - "+ secondNumber + " = " + (firstNumber-secondNumber));
+
+                System.out.println("Finished calculation");
+                printer.println("Do you want to add or subtract?");
+                printer.println("1. Add");
+                printer.println("2. Subtract");
+                printer.println("3. Exit");
+                operation = reader.readLine();
+            }
+
+            /* Close connection */
+            reader.close();
+            printer.close();
+            connection.close();
+        } catch (IOException ioE){
+            ioE.printStackTrace();
+        }
+    }
+}
---
+++ Øving 3/SimpleWebServer.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+
+public class SimpleWebServer {
+    public static void main(String[] args) throws IOException {
+        final int PORT_NUMBER = 5080;
+        ServerSocket server = new ServerSocket(PORT_NUMBER);
+        System.out.println("Server started on port " + PORT_NUMBER);
+
+        while (true) {
+            Socket socket = server.accept();
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-Type: text/html; charset=utf-8");
+            out.println();
+
+            out.println("<HTML><BODY>");
+            out.println("<H1>Greetings. You have connected to my simple web server</H1>");
+            out.println("Header from client:");
+            out.println("<UL>");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null && !line.equals("")) {
+                out.println("<LI>" + line + "</LI>");
+            }
+            out.println("</UL>");
+            out.println("</BODY></HTML>");
+            socket.close();
+        }
+    }
+}
