361f014535c8fb6d5318db62ff038742e0273f36
Aleksander Olsvik <alekol@ntnu.no>
2023-03-14 13:13:19 +0100

    Better display of messages

--- Øving 6/WebSocket/web.js
+++ Øving 6/WebSocket/web.js
@@ -2,27 +2,26 @@ const net = require('net');
 const crypto = require('crypto')
 
 const connections=[]
+const messages= []
 
-// Incomplete WebSocket server
 const wsServer = net.createServer((connection) => {
     console.log('Client connected');
     connection.on('data', (data) => {
-      console.log(data)
-      console.log('Data received from client:\n', data.toString());
       if(data.toString().split('\n')[0].includes('GET / HTTP')){
-			  const info = data.toString().split('\n')
+		const info = data.toString().split('\n')
         var result = ""
-			  for(let i = 0; i<info.length; i++){
-			  	if(info[i].includes('Sec-WebSocket-Key')){
-			  		result = info[i]
-			  	}
-			  }
-      
-			  const clientKey = result.split(':')[1].trim()
-			  const magicString = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
-    	  const serverKey = crypto.createHash('sha1')
-    		  .update(clientKey + magicString)
-      	  .digest('base64');
+		for(let i = 0; i<info.length; i++){
+			if(info[i].includes('Sec-WebSocket-Key')){
+				result = info[i]
+			}
+		}
+
+		const clientKey = result.split(':')[1].trim()
+		const magicString = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+    	const serverKey = crypto
+			.createHash('sha1')
+			.update(clientKey + magicString)
+			.digest('base64');
     
   	  	const response = `HTTP/1.1 101 Switching Protocols\r\n`
     	  	  + `Upgrade: websocket\r\n`
@@ -32,8 +31,9 @@ const wsServer = net.createServer((connection) => {
     
         connection.write(response)
         connections.push(connection)
-        console.log('\n' + response)
-    
+		for (let i = 0; i < messages.length; i++) {
+			connection.write(createWebSocketFrame(messages[i]))
+		}
 		}
 		else{
 			let length = data[1] & 127;
@@ -45,7 +45,9 @@ const wsServer = net.createServer((connection) => {
 				message+=String.fromCharCode(byte)
 			}
       try{
-        const messageToClient = prepareMessage(message)
+		const sendMessage = JSON.parse(message).text
+		messages.push(sendMessage)
+        const messageToClient = createWebSocketFrame(sendMessage)
         for (let index = 0; index < connections.length; index++) {
           connections[index].write(messageToClient)
         }
@@ -66,26 +68,16 @@ const wsServer = net.createServer((connection) => {
     console.log('WebSocket server listening on port 3001');
   });
 
-  function prepareMessage(message){
-    const msg = Buffer.from(message)
-    const msgSize = msg.length
 
-    let dataFrameBuffer
-
-    const firstByte = 0x80 | 0x01
-    if (msgSize <= 125){
-        const bytes = [firstByte]
-        dataFrameBuffer = Buffer.from(bytes.concat(msgSize))
-    } else {
-        throw new Error("Message too long.")
-    }
-    const totalLength = dataFrameBuffer.byteLength + msgSize
-
-    const dataFrameResponse = Buffer.allocUnsafe(totalLength)
-    let offset = 0
-    for (const buffer of [dataFrameBuffer, msg]){
-        dataFrameResponse.set(buffer, offset)
-        offset += buffer.length
-    }
-    return dataFrameResponse
-}
+  function createWebSocketFrame(message) {
+	const msg = Buffer.from(message);
+	const msgSize = msg.length;
+  
+	const header = Buffer.alloc(2);
+	header.writeUInt8(0x81, 0);
+	header.writeUInt8(msgSize, 1);
+  
+	const frame = Buffer.concat([header, msg]);
+	return frame;
+  }
+  
